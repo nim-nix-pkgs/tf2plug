200001111039315a36e4f22230fae544279381cb
hugosenari <hugosenari@gmail.com>
2022-03-12 03:33:50 -0300

    chore: re index tf2plug

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''A mod manager for TF2'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."tf2plug-master".url = "path:./master";
+  inputs."tf2plug-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."tf2plug-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''A mod manager for TF2'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-tf2plug-master.flake = false;
+  inputs.src-tf2plug-master.owner = "waylon531";
+  inputs.src-tf2plug-master.ref   = "refs/heads/master";
+  inputs.src-tf2plug-master.repo  = "tf2plug";
+  inputs.src-tf2plug-master.type  = "gitlab";
+  
+  inputs."docopt".dir   = "nimpkgs/d/docopt";
+  inputs."docopt".owner = "riinr";
+  inputs."docopt".ref   = "flake-pinning";
+  inputs."docopt".repo  = "flake-nimble";
+  inputs."docopt".type  = "github";
+  inputs."docopt".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."docopt".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-tf2plug-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-tf2plug-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
